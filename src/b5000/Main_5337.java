ff69dee6d916a71ac6d7759c3471b5410cd96cb9
unknown <dlgusdn978@naver.com>
2023-12-11 23:35:40 +0900

    BOJ 5337

---
+++ src/b5000/Main_5337.java
@@ -0,0 +1,16 @@
+package b5000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
+

b06d814026f1ee32986aef36189bb8bad65639e1
unknown <dlgusdn978@naver.com>
2023-12-12 23:08:56 +0900

    BOJ 2738

---
+++ src/b2000/Main_2738.java
@@ -0,0 +1,34 @@
+package b2000;
+
+import java.util.*;
+import java.io.*;
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                int temp = Integer.parseInt(st.nextToken());
+                arr[i][j] += temp;
+            }
+        }
+        for(int[] a : arr){
+            for(int b : a){
+                System.out.print(b+" ");
+            }
+            System.out.println();
+        }
+    }
+}

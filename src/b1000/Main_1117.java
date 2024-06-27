7832c20bb8b8afe484daf198df1209a2cb9f289f
dlgusdn978 <dlgusdn978@naver.com>
2024-06-27 18:58:58 +0900

    BOJ Impl

--- src/b1000/Main_1117.java
+++ src/b1000/Main_1117.java
@@ -3,7 +3,9 @@ package b1000;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main_1117 {
@@ -13,24 +15,22 @@ public class Main_1117 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		// 너비
-		int w = Integer.parseInt(st.nextToken());
+		long w = Long.parseLong(st.nextToken());
 		// 높이
-		int h = Integer.parseInt(st.nextToken());
+		long h = Long.parseLong(st.nextToken());
 		// 접는 위치
-		int f = Integer.parseInt(st.nextToken());
+		long f = Long.parseLong(st.nextToken());
 		// 세로 접는 횟수
-		int c = Integer.parseInt(st.nextToken());
+		long c = Long.parseLong(st.nextToken());
 
 		// 좌표값
-		int[][] cor = new int[2][2];
-		for(int i=0; i<2; i++) {
-			for(int j=0; j<2; j++) {
-				cor[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
+		long x1 = Long.parseLong(st.nextToken());
+		long y1 = Long.parseLong(st.nextToken());
+		long x2 = Long.parseLong(st.nextToken());
+		long y2 = Long.parseLong(st.nextToken());
+
 		// f 값이 r/2보다 크다면 f=r-f
-		if(f>w/2) f = w-f;
+		if(f>w/2) f= w-f;
 		// 각 좌표에 1 채움.
 //		int[][] arr = new int[h][w];
 //		for(int i=0; i<h; i++) {
@@ -40,13 +40,14 @@ public class Main_1117 {
 		long area = w*h;
 		long colored = 0;
 
-		colored += (cor[1][0]-cor[0][0])*(cor[1][1]-cor[0][1])*(c+1);
-		if(f<=cor[0][0]) {
-			colored = area-colored;
+		if(f<=x1) {
+			colored = (x2-x1)*(y2-y1)*(c+1);
+		}else if(f<x2) {
+			colored = (y2-y1)*(c+1)*( (f-x1)*2+(x2-f) );
 		}else {
-			colored = area - (colored+(Math.min(f, cor[1][0])-cor[0][0])*(cor[1][1]-cor[0][1])*(c+1));
+			colored = (x2-x1)*(y2-y1)*(c+1)*2;
 		}
-		System.out.println(colored);
+		System.out.println(area-colored);
 
 	}
 

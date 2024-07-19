7b99594231817625d9fbf6edd7d47316c4a2702e
dlgusdn978 <dlgusdn978@naver.com>
2024-07-19 23:48:09 +0900

    BOJ

---
+++ src/b5000/Main_5399.java
@@ -0,0 +1,19 @@
+package b5000;
+
+public class Main_5399 {
+
+	public static void main(String[] args) {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+
+	}
+
+}

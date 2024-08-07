108b8c4e6a956fbd670a6c5a5cad633ef770b9e3
dlgusdn978 <dlgusdn978@naver.com>
2024-08-07 21:48:03 +0900

    BOJ

---
+++ src/b14000/Main_14581.java
@@ -0,0 +1,17 @@
+package b14000;
+
+import java.util.Scanner;
+
+public class Main_14581 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String name = sc.next();
+		System.out.println(":fan::fan::fan:");
+		System.out.println(":fan::"+name+"::fan:");
+		System.out.println(":fan::fan::fan:");
+
+	}
+
+}

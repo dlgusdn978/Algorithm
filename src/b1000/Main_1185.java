ad69fdfaf443658c486ea60b1c94cedefe810311
dlgusdn978 <dlgusdn978@naver.com>
2025-04-24 14:07:35 +0900

    BOJ MST

--- src/b1000/Main_1185.java
+++ src/b1000/Main_1185.java
@@ -11,48 +11,46 @@ import java.util.StringTokenizer;
 
 public class Main_1185 {
 
-	static int n, p;
-	static int[] visitCost;
-	static List<Node> list;
-	static int[] parents;
 	static class Node{
-		int from;
-		int to;
+		int start;
+		int end;
 		int weight;
-		public Node(int from, int to, int weight) {
-			this.from = from;
-			this.to = to;
+		public Node(int start, int end, int weight) {
+			this.start = start;
+			this.end = end;
 			this.weight = weight;
 		}
 	}
+	static int[] parents;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer  st = new StringTokenizer(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		// 방문할 나라의 수 n
-		n = Integer.parseInt(st.nextToken());
-		// 연결하는 길의 수 p;
-		p = Integer.parseInt(st.nextToken());
-
-		// 각 나라 방문 비용
-		visitCost = new int[n+1];
+		// 모든 도시를 최소 한번 이상 방문
+		// 첫 시작 나라 = 마지막 도착 나라
+		// 길을 통과하기 위한 비용과, 각 나라에 도착했을 때 내는 비용이 각각 다름
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		// 각 나라 도착 시 지불할 비용
+		int[] cost = new int[n+1];
 		for(int i=1; i<=n; i++) {
-			visitCost[i] = Integer.parseInt(br.readLine());
+			cost[i] = Integer.parseInt(br.readLine());
 		}
-		list = new ArrayList<Node>();
-		// 리스트 초기화
-//		for(int i=1; i<=n; i++) {
-//			list[i] = new ArrayList<>();
-//		}
-		// 각 나라 간 이동 비용 입력
-		for(int i=1; i<=n; i++) {
+		List<Node> list = new ArrayList<>();
+		
+		// 각 나라 간 이동 비용
+		for(int i=0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
-			list.add(new Node(start, end, weight+visitCost[end]));
-			list.add(new Node(end, start, weight+visitCost[start]));
+			// 각 나라 간 이동 비용은 왕복하는 것까지 생각해서 (이동비용*2+각 나라 도착비용)?
+			
+			list.add(new Node(start, end, weight*2+cost[start]+cost[end]));
 		}
+		
+		makeSet(n);
 		
 		Collections.sort(list, new Comparator<Node>() {
 			@Override
@@ -60,31 +58,48 @@ public class Main_1185 {
 				return o1.weight-o2.weight;
 			}
 		});
-		
-		makeSet();
-		
-		int count = 0;
-		int total = 0;
-		
-		for(Node node : list) {
-			if(union(node.from, node.to)) {
-				total += node.weight;
-				System.out.println(node.from+" "+node.to+" 연결, 비용 : "+node.weight+", 연결 결과 : "+total);
-				if(++count==n-1) break;
+		int[] visited = new int[n+1];
+		int start = 0;
+		int sum = 0;
+		int cnt = 0;
+		for(int i=0; i<list.size(); i++) {
+			Node cur = list.get(i);
+			int from = cur.start;
+			int to = cur.end;
+			int weight = cur.weight;
+			
+			if(i==0) start = from;
+			
+			if(union(from, to)) {
+				visited[from]++;
+				visited[to]++;
+//				System.out.println(from+" "+to+" "+weight);
+				sum += weight;
+				if(++cnt==n-1) break;
 			}
 		}
+		int min = Integer.MAX_VALUE;
+		for(int i=0; i<visited.length; i++) {
+			if(visited[i]>1) {
+				min = Math.min(min, cost[i]);
+			}
+		}
+//		System.out.println(start);
+		System.out.println(sum+min);
 	}
 	
-	static void makeSet() {
+	static void makeSet(int n) {
 		parents = new int[n+1];
-		for(int i=1; i<=n; i++) {
+		for(int i=1; i<parents.length; i++) {
 			parents[i] = i;
 		}
 	}
+	
 	static int findSet(int v1) {
-		if(v1==parents[v1]) return v1;
+		if(parents[v1]==v1) return v1;
 		else return parents[v1] = findSet(parents[v1]);
 	}
+	
 	static boolean union(int v1, int v2) {
 		int aRoot = findSet(v1);
 		int bRoot = findSet(v2);
